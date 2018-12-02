e425e0268e836ac64b57cc1d376af950c5fd946d
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-12-01 23:17:55 -0200

    Solution to the second part of day 1 of 2018

--- 2018/day01/solution.py
+++ 2018/day01/solution.py
@@ -5,6 +5,18 @@ def freq_accum(incs):
     return sum(incs)
 
 
+def first_repeated_freq(incs):
+    freqs = set([])
+    freq = 0
+    i = 0
+    while True:
+        if freq in freqs:
+            return freq
+        freqs.add(freq)
+        freq += incs[i]
+        i = (i + 1) % len(incs)
+
+
 def main():
     incs = []
     while True:
@@ -15,6 +27,7 @@ def main():
             break
 
     print('Resulting frequency:', freq_accum(incs))
+    print('First repeating frequency:', first_repeated_freq(incs))
 
 
 if __name__ == '__main__':
