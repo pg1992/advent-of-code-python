9c8960364d337d36d9b46abf014bcfea21146caf
Pedro Guilherme Siqueira Moreira <pedro.eletronica.unb@gmail.com>
2018-12-05 15:13:37 -0200

    Solution to day 2 part 2 of 2018

--- 2018/day02/solution.py
+++ 2018/day02/solution.py
@@ -8,6 +8,18 @@ def count_repetitions(word):
     return counts
 
 
+def find_similar(word_list):
+    total_words = len(word_list)
+    for i in range(total_words):
+        for j in range(i + 1, total_words):
+            zipped = zip(word_list[i], word_list[j])
+            diff_list = [i for i, k in enumerate(zipped) if k[0] != k[1]]
+            ndiff = len(diff_list)
+            if ndiff == 1:
+                diff_idx = diff_list[0]
+                return word_list[i][:diff_idx] + word_list[i][diff_idx + 1:]
+
+
 def main():
     ids = []
     while True:
@@ -20,6 +32,7 @@ def main():
     threes = list(filter(lambda s: 3 in count_repetitions(s), ids))
 
     print('Checksum:', len(twos) * len(threes))
+    print('Similar ID:', find_similar(ids))
 
 
 if __name__ == '__main__':
