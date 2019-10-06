908737f3c565486a9c2c79bb3721d8f68cde6cad
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2019-10-05 21:51:52 -0300

    Solution to the second part of day 2 of 2015 in python

--- 2015/python/02/solution.py
+++ 2015/python/02/solution.py
@@ -7,14 +7,21 @@ Solution for day 2 of 2015
 import sys
 
 
-def find_total(data):
-    """Find the total area of paper needed"""
-    total = 0
+def find_totals(data):
+    """Find the total area of paper needed and ribbon length"""
+    total_area = 0
+    total_length = 0
+
     for line in data.rstrip().split('\n'):
         l, h, w = map(int, line.split('x'))
+
         areas = [l*h, l*w, h*w]
-        total += 2 * sum(areas) + min(areas)
-    return total
+        total_area += 2 * sum(areas) + min(areas)
+
+        perimeters = [2 * (l + h), 2 * (l + w), 2 * (h + w)]
+        total_length += min(perimeters) + l * h * w
+
+    return total_area, total_length
 
 
 def main():
@@ -25,9 +32,9 @@ def main():
     with open(filename, 'r') as file_pointer:
         data = file_pointer.read()
 
-    total_area = find_total(data)
+    total_area, total_length = find_totals(data)
 
-    print(total_area)
+    print(total_area, total_length)
 
 
 if __name__ == '__main__':
